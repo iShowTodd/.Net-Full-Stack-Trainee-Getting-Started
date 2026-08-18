9e6a44a9107378b7dbfaae88468d054bd6d89746
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-18 11:48:03 +0200

    Implementing : Exploring how tasks return values

---
+++ Advanced-CSharp/Asynchronous Programming/TaskReturnsValue/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskReturnsValue
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task<DateTime> task = Task.Run(() => DateTime.Now);
+            Console.WriteLine(task.Result); // Blocks the Thread until result is ready
+
+            Console.WriteLine(task.GetAwaiter().GetResult());
+        }
+
+        private static DateTime GetCurrentDateTime() => DateTime.Now;
+    }
+}
