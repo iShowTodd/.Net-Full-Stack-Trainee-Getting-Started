638a026fae0d0739b61fbe74e3c034c3920d6664
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-18 12:58:03 +0200

    Implementing : Exploring the Async vs Sync Code

---
+++ Advanced-CSharp/Asynchronous Programming/SyncVsAsync/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SyncVsAsync
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ShowThreadInfo(Thread.CurrentThread, 7);
+            CallSynchronous();
+
+            ShowThreadInfo(Thread.CurrentThread, 10);
+            CallAsynchronous();
+
+            ShowThreadInfo(Thread.CurrentThread, 13);
+            Console.ReadKey();
+        }
+
+        private static void CallSynchronous()
+        {
+            Thread.Sleep(4000);
+            ShowThreadInfo(Thread.CurrentThread, 20);
+            Task.Run(() => Console.WriteLine("++++++++++ Synchronous +++++++++++")).Wait();
+        }
+
+        private static void CallAsynchronous()
+        {
+            ShowThreadInfo(Thread.CurrentThread, 26);
+            Task.Delay(4000).GetAwaiter().OnCompleted(() =>
+            {
+                ShowThreadInfo(Thread.CurrentThread, 29);
+                Console.WriteLine("++++++++++ Asynchronous +++++++++++");
+            });
+        }
+
+        private static void ShowThreadInfo(Thread th, int line)
+        {
+            Console.WriteLine($"Line#: {line}, TID: {th.ManagedThreadId}, Pooled: {th.IsThreadPoolThread}, Background: {th.IsBackground}");
+        }
+    }
+}
