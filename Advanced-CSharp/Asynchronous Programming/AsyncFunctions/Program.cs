b8cdfec64d546d6012b80f2feccc208d70295199
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-18 14:31:42 +0200

    Implementing : Exploring Async Functions

---
+++ Advanced-CSharp/Asynchronous Programming/AsyncFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AsyncFunctions
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var task = Task.Run(() => ReadContent("www.google.com"));
+
+            var awaiter = task.GetAwaiter();
+            awaiter.OnCompleted(() => Console.WriteLine(awaiter.GetResult));
+
+            Console.WriteLine(await ReadContentAsync("https://www.youtube.com/c/Metigator"));
+
+            Console.ReadKey();
+        }
+
+        private static Task<string> ReadContent(string url)
+        {
+            var client = new HttpClient();
+
+            var task = client.GetStringAsync(url);
+
+            return task;
+        }
+
+        private static async Task<string> ReadContentAsync(string url)
+        {
+            var client = new HttpClient();
+
+            var content = await client.GetStringAsync(url);
+
+            return content;
+        }
+    }
+}
