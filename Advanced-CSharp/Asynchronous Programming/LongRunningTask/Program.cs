b26f01c5cdf4ee85560bf5311f6ee00aac17b8e2
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-18 11:58:37 +0200

    Implementing : Exploring Long running Tasks

---
+++ Advanced-CSharp/Asynchronous Programming/LongRunningTask/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LongRunningTask
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // How to Run a Long Running Task
+            var task = Task.Factory.StartNew(() => RunLongTask(), TaskCreationOptions.LongRunning);
+
+            Console.ReadKey();
+        }
+
+        private static void RunLongTask()
+        {
+            Thread.Sleep(3000);
+            ShowThreadInfo(Thread.CurrentThread);
+            Console.WriteLine("Completed");
+        }
+
+        private static void ShowThreadInfo(Thread th)
+        {
+            Console.WriteLine($"TID: {th.ManagedThreadId}, Pooled: {th.IsThreadPoolThread}, Background: {th.IsBackground}");
+        }
+    }
+}
