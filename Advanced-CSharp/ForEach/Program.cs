5ab0692f9eecc9d3ce1f0a015b96dc6f890f6a45
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 14:35:40 +0200

    Implementig : Deep dive into Yield Key word

--- Advanced-CSharp/ForEach/Program.cs
+++ Advanced-CSharp/ForEach/Program.cs
@@ -2,9 +2,43 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("\n\n Using For");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($" {numbers[i]}");
+            }
+
+            Console.WriteLine("\n\n Using Foreach");
+            foreach (var n in numbers)
+            {
+                Console.Write($" {n}");
+            }
+
+            ForEach(numbers);
+        }
+
+        private static void ForEach<T>(IEnumerable<T> source)
+        {
+            IEnumerator<T> enumerator = source.GetEnumerator();
+            IDisposable disposable;
+            try
+            {
+                T item;
+                while (enumerator.MoveNext())
+                {
+                    item = enumerator.Current;
+                    Console.Write($" {item}");
+                }
+            }
+            finally
+            {
+                disposable = enumerator;
+                disposable.Dispose();
+            }
         }
     }
 }
--- Advanced-CSharp/Yield/Program.cs
+++ Advanced-CSharp/Yield/Program.cs
@@ -4,39 +4,55 @@
     {
         private static void Main(string[] args)
         {
-            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Console.WriteLine("using GenerateV1()");
+            //foreach (var n in GenerateV1())
+            //{
+            //    Console.Write($" {n}");
+            //}
 
-            Console.WriteLine("\n\n Using For");
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Console.Write($" {numbers[i]}");
-            }
+            //Console.WriteLine("\n\nusing GenerateV2()");
+            //foreach (var n in GenerateV2())
+            //{
+            //    Console.Write($" {n}");
+            //}
 
-            Console.WriteLine("\n\n Using Foreach");
-            foreach (var n in numbers)
+            Console.WriteLine("\n\nusing GenerateV3()");
+            foreach (var n in GenerateV3())
             {
                 Console.Write($" {n}");
             }
+            Console.ReadKey();
         }
 
-        private static void ForEach<T>(IEnumerable<T> source)
+        private static IEnumerable<int> GenerateV1()
         {
-            IEnumerator<T> enumerator = source.GetEnumerator();
-            IDisposable disposable;
-            try
+            var result = new List<int>();
+            for (int i = 1; i <= 5; i++)
             {
-                T item;
-                while (enumerator.MoveNext())
-                {
-                    item = enumerator.Current;
-                    Console.Write($" {item}");
-                }
+                result.Add(i);
             }
-            finally
+            return result;
+        }
+
+        private static IEnumerable<int> GenerateV2()
+        {
+            for (int i = 1; i <= 5; i++)
             {
-                disposable = enumerator;
-                disposable.Dispose();
+                yield return i;
             }
+        }
+
+        private static IEnumerable<int> GenerateV3()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield break;
+            yield return 6;
+            yield return 7;
+            yield return 8;
         }
     }
 }
