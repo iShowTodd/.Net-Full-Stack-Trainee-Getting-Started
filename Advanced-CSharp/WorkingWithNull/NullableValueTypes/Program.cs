5ff2815627de0576c7316287169b6a08a2df8abf
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 19:33:06 +0300

    Implementing : Deep dive into Null Value Types

---
+++ Advanced-CSharp/WorkingWithNull/NullableValueTypes/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace NullableValueTypes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int mark1 = 15;
+            int mark2 = default; // 0
+
+            Nullable<int> mark3 = default; // Default here is null
+
+            if (mark3 is null)
+            {
+                Console.WriteLine("Mark is not available");
+            }
+            else
+            {
+                Console.WriteLine($"mark3 = {mark3}");
+            }
+
+            int? mark4 = default; // Null similar to Nullable<int>
+
+            if (!mark4.HasValue)
+            {
+                Console.WriteLine("Mark is not available");
+            }
+            else
+            {
+                Console.WriteLine($"mark4 = {mark4}");
+            }
+
+            Nullable<int> mark5 = default; // null
+            Console.WriteLine($"mark5 = {(mark5.HasValue ? mark5 : "null")}");
+
+            Nullable<int> mark6 = new Nullable<int>(); // null
+            Console.WriteLine($"mark6 = {(mark6.HasValue ? mark6 : "null")}");
+
+            int? mark7 = default(int?); // null
+            Console.WriteLine($"mark7 = {(mark7.HasValue ? mark7 : "null")}");
+
+            Nullable<int> mark8 = new(); // 0
+            Console.WriteLine($"mark8 = {(mark8.HasValue ? mark8 : "null")}");
+        }
+    }
+}
