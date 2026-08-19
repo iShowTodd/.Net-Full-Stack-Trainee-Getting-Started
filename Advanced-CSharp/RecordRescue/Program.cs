67ef732a696a869ad8ea2064c9b4463f0047beb2
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 15:50:44 +0200

    Implementing : intro to Records

---
+++ Advanced-CSharp/RecordRescue/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace RecordRescue
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var p1 = new Point(2, 3);
+            var p2 = new Point(2, 3);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+            Console.WriteLine($"p1.Equals(p2): {p1.Equals(p2)}");
+            Console.WriteLine($"(p1 == p2): {p1 == p2}");
+        }
+    }
+
+    internal record Point
+    {
+        // override object equal
+        // implement IEquatable <Point>
+        // override object GetHashCode
+        // override ==, !=
+        // override ToString();
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
