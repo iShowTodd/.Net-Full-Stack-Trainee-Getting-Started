0544759bd181ecee0afe6e14975325572e20a06f
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 15:59:39 +0200

    Implementing : exploring the positional record

---
+++ Advanced-CSharp/PositionalRecord/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PositionalRecord
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var p1 = new Point(2, 3);
+            // p1.X = 10; // position record are immutable by default
+            Console.WriteLine(p1);
+            var (x, y) = p1;
+
+            Console.WriteLine($"x = {x}, y = {y}");
+
+            var p2 = new Point { X = 10, Y = 11 };
+            Console.WriteLine(p2);
+            Console.ReadKey();
+        }
+    }
+
+    // record Point (int X, int Y);
+
+    internal record Point(int X, int Y)
+    {
+        public Point() : this(0, 0)
+        {
+        }
+    }
+}
---
+++ Advanced-CSharp/StructRecord/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StructRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
