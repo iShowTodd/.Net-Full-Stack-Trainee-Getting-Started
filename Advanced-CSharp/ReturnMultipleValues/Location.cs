684253622d450d08ee500b9ae27034732b8ae337
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-20 01:39:28 +0300

    Implementing : returning multiple types

---
+++ Advanced-CSharp/ReturnMultipleValues/Location.cs
@@ -0,0 +1,13 @@
+﻿namespace ReturnMultipleValues
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public double DistanceInKm { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ....... {DistanceInKm:F2} km";
+        }
+    }
+}
---
+++ Advanced-CSharp/ReturnMultipleValues/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ReturnMultipleValues
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Using Class DS");
+            var facilityName = FacilityDistanceCalculator.GetFacilityLocationInfoV1("Hospital");
+            Console.WriteLine(facilityName);
+
+            Console.WriteLine();
+            Console.WriteLine("Using out Parameter");
+
+            string name = "";
+            double distanceInKm = 0;
+
+            FacilityDistanceCalculator
+                .GetFacilityLocationInfoV2("Hospital", out name, out distanceInKm);
+
+            Console.WriteLine($"{name} ....... {distanceInKm:F2} km");
+            Console.ReadKey();
+        }
+    }
+}
