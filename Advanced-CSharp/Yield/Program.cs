edddbd92f07ccc542e04365ff8dfe17de352269a
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 14:24:54 +0200

    Fixing : reparing the wrong namespace and folder names + adding a implementation to the new function

---
+++ Advanced-CSharp/Yield/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Yield
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("\n\n Using For");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($" {numbers[i]}");
+            }
+
+            Console.WriteLine("\n\n Using Foreach");
+            foreach (var n in numbers)
+            {
+                Console.Write($" {n}");
+            }
+        }
+
+        private static void ForEach<T>(IEnumerable<T> source)
+        {
+            IEnumerator<T> enumerator = source.GetEnumerator();
+            IDisposable disposable;
+            try
+            {
+                T item;
+                while (enumerator.MoveNext())
+                {
+                    item = enumerator.Current;
+                    Console.Write($" {item}");
+                }
+            }
+            finally
+            {
+                disposable = enumerator;
+                disposable.Dispose();
+            }
+        }
+    }
+}
