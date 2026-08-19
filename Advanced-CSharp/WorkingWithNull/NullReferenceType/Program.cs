2ca11e6899cc0fcb1d550009a5731fa1810541fa
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 20:35:23 +0300

    Implementing : intro to Null Reference types

---
+++ Advanced-CSharp/WorkingWithNull/NullReferenceType/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace NullReferenceType
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string? name = null;
+
+            string decision = IsLongName(name) ? "Long" : "Short";
+
+            Console.WriteLine($"{name} is {decision}");
+            Console.ReadKey();
+        }
+
+        private static bool IsLongName(string? name)
+        {
+            // avoiding null
+            if (name is null)
+                return false;
+
+            return name.Length > 10; // assumption of name is not null
+        }
+    }
+}
