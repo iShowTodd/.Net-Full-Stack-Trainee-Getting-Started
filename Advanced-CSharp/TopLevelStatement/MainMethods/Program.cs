0d90ef5ac1b94503f5c8b26af6a7cfa8a263a3b4
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 16:25:59 +0200

    Implementing : intro to the world before .Net 6

---
+++ Advanced-CSharp/TopLevelStatement/MainMethods/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace MainMethods
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Metigator");
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Metigator");
+        }
+
+        //static int Main()
+        //{
+        //    Console.WriteLine("Metigator");
+        //    return 0;
+        //}
+
+        //static int Main(string[] args)
+        //{
+        //    Console.WriteLine("Metigator");
+        //    return 0;
+        //}
+
+        //static async Task Main(string[] args)
+        //{
+        //    await Task.Run(() => Console.WriteLine("Metigator"));
+        //}
+
+        //static async Task<int> Main(string[] args)
+        //{
+        //    await Task.Run(() =>
+        //    {
+        //        Console.WriteLine("Metigator");
+        //    });
+        //    return 0;
+        //}
+    }
+}
