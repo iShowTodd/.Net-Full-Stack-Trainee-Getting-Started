648f2c13c0ab113ccea3f0e1bac484dda82a1601
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 22:05:50 +0300

    Implementing : intro to Null and Generics

---
+++ Advanced-CSharp/WorkingWithNull/NullableAndGenerics/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NullableAndGenerics
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static T? DoSomething<T>(T source)
+        {
+            return source;
+        }
+    }
+}
