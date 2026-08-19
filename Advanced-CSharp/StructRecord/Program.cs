04ac1946d250e5ecb20f9ceae56b07073f4b268e
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 16:06:15 +0200

    Implementing : Exploring Struct Record

--- Advanced-CSharp/StructRecord/Program.cs
+++ Advanced-CSharp/StructRecord/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var p1 = new Point(2, 3);
+
+            // p1.X = 10; // position readonly struct record are immutable
+            Console.WriteLine(p1);
+
+            Console.ReadKey();
         }
+    }
+
+    public readonly record struct Point(int X, int Y);
+
+    // Postional Record are mutable
+    public record struct PointV2
+    {
+        public int X;
+        public int Y;
     }
 }
