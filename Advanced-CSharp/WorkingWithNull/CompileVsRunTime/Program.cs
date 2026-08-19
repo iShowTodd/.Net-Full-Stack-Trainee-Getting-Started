e4b51c7cb88dd3acec4bcaffb91dab640ae10a2a
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-19 22:19:10 +0300

    Implementing : intro to the ascii code of the alphabitic characters

--- Advanced-CSharp/WorkingWithNull/CompileVsRunTime/Program.cs
+++ Advanced-CSharp/WorkingWithNull/CompileVsRunTime/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "123A";
+            //int num1 = input; // CS0029 Can not convert string to int (Error at Compile time)
+            // Compile Time : building source code and convert it to IL
+            // Finding any error at this phase is easy
+
+            // Run Time : when you execute the code "click the run button"
+            int num2 = int.Parse(input); // RunTime Error
         }
     }
 }
