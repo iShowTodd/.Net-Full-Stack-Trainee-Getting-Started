606b3fbde1542a05ae4df5cd733ebc9f149f6bf6
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-22 16:01:40 +0300

    Upsolving : Task 3

---
+++ Tasks/Task3/Employee.cs
@@ -0,0 +1,12 @@
+namespace DelegateAndListTask
+{
+  class Employee
+  {
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public decimal Salary { get; set; }
+  }
+
+
+
+}
