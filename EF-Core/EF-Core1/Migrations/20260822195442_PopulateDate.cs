f5debfe71fa3a4f8b2f50d174b2126f69e28a584
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-22 22:59:05 +0300

    Implementing : creating my own migration

---
+++ EF-Core/EF-Core1/Migrations/20260822195442_PopulateDate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core1.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Employees values('Ahmed')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Employees WHERE Name = 'Ahmed'");
+        }
+    }
+}
