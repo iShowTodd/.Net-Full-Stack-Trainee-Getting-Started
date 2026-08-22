8b62948ed2438396060d48430e17fef1946d8372
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-22 22:25:38 +0300

    Implementing : adding my first migration

--- EF-Core/EF-Core1/ApplicationDbContext.cs
+++ EF-Core/EF-Core1/ApplicationDbContext.cs
@@ -4,9 +4,8 @@ namespace EF_Core1
 {
     public class ApplicationDbContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
-        }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EF-Core;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
     }
 }
---
+++ EF-Core/EF-Core1/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace EF_Core1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
