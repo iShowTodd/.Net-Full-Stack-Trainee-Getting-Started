dc62f199b4ef1e9d4d468ccf59f68ee2d4e7aaa2
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-22 14:40:15 +0300

    Upsolving : task2

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace StructAndArrayTask
+{
+
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Employee[]? emps = new Employee[3];
+
+      for (int i = 0; i < emps.Length; ++i)
+      {
+        emps[i].Id = i + 1;
+
+        string? name = Console.ReadLine();
+        if (name is not null)
+        {
+          emps[i].Name = name;
+        }
+      }
+
+      foreach (var emp in emps)
+      {
+        Console.WriteLine($" Employee # {emp.Id} : id :  {emp.Id} , Name : {emp.Name}");
+      }
+
+    }
+  }
+
+  struct Employee
+  {
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+  }
+}
