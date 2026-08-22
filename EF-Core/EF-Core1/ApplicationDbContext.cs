04475b4373d94072cd1942ca0c5c04211f91fb03
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-22 21:59:10 +0300

    Setting up : setting the packages and connection string for EF-Core

---
+++ EF-Core/EF-Core1/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
+        }
+    }
+}
---
+++ EF-Core/EF-Core1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EF_Core1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
