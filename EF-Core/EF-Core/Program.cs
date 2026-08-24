a144db869c35e5106fd391c480b167212444fb13
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-24 22:41:35 +0300

    Implementing : selecting single element using single

--- EF-Core/EF-Core/Program.cs
+++ EF-Core/EF-Core/Program.cs
@@ -9,6 +9,9 @@
             var posts = _context.Posts.ToList(); // selecting all items
             var post = _context.Posts.Find(1); // selecting by PK
 
+            var post2 = _context.Posts.Single(p => p.Id == 1); //  Selecting single element using single
+            var post3 = _context.Posts.SingleOrDefault(p => p.Id == 1); //  Selecting single element using singleOrDefault
+
             // _context.Blogs.Add(new Blog() { AddedOn = DateTime.Now, Url = "test.com" });
             // _context.SaveChanges();
 
