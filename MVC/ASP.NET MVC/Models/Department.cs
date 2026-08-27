491cbfbbd427ab5a119693a6f4c6645fc234e67f
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-27 14:38:46 +0300

    Implementing : adding employee model

--- MVC/ASP.NET MVC/Models/Department.cs
+++ MVC/ASP.NET MVC/Models/Department.cs
@@ -1,8 +1,11 @@
-﻿namespace ASP.NET_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_MVC.Models
 {
     public class Department
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
+
         public string Name { get; set; }
         public string ManagerName { get; set; }
     }
---
+++ MVC/ASP.NET MVC/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP.NET_MVC.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string? Image { get; set; }
+        public string Address { get; set; }
+
+        [ForeignKey("Department")]
+        public int DeptId { get; set; }
+
+        public virtual Department? Department { get; set; }
+    }
+}
