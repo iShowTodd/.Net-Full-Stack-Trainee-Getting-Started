5572c42b8c48102df1a5c7750c6f38c211e6caf9
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-27 14:29:49 +0300

    Implementing : adding the department model

---
+++ MVC/ASP.NET MVC/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_MVC.Models
+{
+    public class Department
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ManagerName { get; set; }
+    }
+}
