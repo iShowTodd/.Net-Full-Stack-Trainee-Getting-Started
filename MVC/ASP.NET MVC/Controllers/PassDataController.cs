e69cf9d30025fb38da65fad0262748bbaa7d6cce
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-28 19:54:06 +0300

    Implementing : adding PassDataController

---
+++ MVC/ASP.NET MVC/Controllers/PassDataController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_MVC.Controllers
+{
+    public class PassDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
