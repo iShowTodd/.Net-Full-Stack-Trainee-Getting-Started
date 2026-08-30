bafce5f2ed01b00a1845ee151f0083eeac86d850
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-30 15:06:05 +0300

    Implmeneting and Exploring the StateManagement

---
+++ MVC/ASP.NET MVC/Controllers/StateManagementController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_MVC.Controllers
+{
+    public class StateManagementController : Controller
+    {
+        public IActionResult SetTempData()
+        {
+            TempData["msg"] = "Hello"; // Just One Request then it get destroied
+            return Content("DataSaved");
+        }
+
+        public IActionResult GetOne()
+        {
+            string? message = TempData["msg"].ToString();
+            return Content("get1" + message);
+        }
+
+        public IActionResult GetTwo()
+        {
+            string? message = TempData["msg"].ToString();
+            return Content("get2" + message);
+        }
+    }
+}
