5a7f8bb9c38dee77092f389af183172111f1f411
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-08-30 16:12:56 +0300

    Implementing : Exploring how to mange the storing item

--- MVC/ASP.NET MVC/Program.cs
+++ MVC/ASP.NET MVC/Program.cs
@@ -7,7 +7,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        builder.Services.AddControllersWithViews();
+        builder.Services.AddControllersWithViews().AddSessionStateTempDataProvider();
 
         var app = builder.Build();
 
